a251783cc76872c68df06a046dd87eac51fd490c
inisik <inisik0820@gmail.com>
2018-05-07 21:38:49 -0400

    Update and rename wwwroot/index.php to public/index.php

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+/**
+ * ElixirPHP.
+ *
+ * Securely code high level web applications using ElixirPHP.
+ *
+ * @author ElixirPHP Contributors <https://github.com/inisik/elixirphp/graphs/contributors>
+ *
+ * @license <https://github.com/inisik/elixirphp/blob/master/LICENSE>.
+ * @link    <https://github.com/inisik/elixirphp>.
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
