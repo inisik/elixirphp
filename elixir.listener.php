c4fe3778c014b73930f7f1983af9e056f21f9cd7
inisik <inisik0820@gmail.com>
2018-05-07 21:14:26 -0400

    Change Doctypes.

--- elixir.listener.php
+++ elixir.listener.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 /**
  * ElixirPHP.
  *
- * Be able to securely code high level web applications using PHP.
+ * Be able to securely code high level web applications using ElixirPHP.
  *
  * @author ElixirPHP Contributors <https://github.com/inisik/elixirphp/graphs/contributors>
  *
